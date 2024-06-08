86dfe9ac1716ed3ddb9f27d3808e01d658149130
Ikaro de Assis Sousa <167935329+duck-developer@users.noreply.github.com>
2024-06-08 20:55:11 -0300

    Add files via upload

---
+++ Among_Us/script.js
@@ -0,0 +1,143 @@
+/*==== PARTES DO CORPO ====*/
+var back = document.querySelector(".backpack")
+var shadow = document.querySelector(".shadow")
+var shadow_1 = document.querySelector(".shadow1")
+var fill = document.querySelector(".fill")
+var leg_left = document.querySelector(".leg-left")
+var left_right = document.querySelector(".leg-right")
+
+/* CORES DO AMONG */
+var Verde = document.querySelector(".verde")
+var Verdef = document.querySelector(".verdef")
+var Azul = document.querySelector(".azul")
+var Amarelo = document.querySelector(".amarelo")
+var Laranja = document.querySelector(".laranja")
+var Preto = document.querySelector(".preto")
+var Branco = document.querySelector(".branco")
+var Roxo = document.querySelector(".roxo")
+var Rosa = document.querySelector(".rosa")
+var Vermelho = document.querySelector(".vermelho")
+var Marrom = document.querySelector(".marrom")
+var Ciano = document.querySelector(".ciano")
+
+
+
+Verde.addEventListener("click",verde)
+Verdef.addEventListener("click",verdef)
+Azul.addEventListener("click", azul)
+Amarelo.addEventListener("click", amarelo)
+Laranja.addEventListener("click", laranja)
+Preto.addEventListener("click", preto)
+Branco.addEventListener("click", branco)
+Roxo.addEventListener("click", roxo)
+Rosa.addEventListener("click", rosa)
+Vermelho.addEventListener("click", vermelho)
+Marrom.addEventListener("click", marrom)
+Ciano.addEventListener("click", ciano)
+
+function verde(){
+    back.style.background = "#00d600"
+    shadow_1.style.background = "#00d600"
+    shadow.style.background ="#009532"
+    fill.style.background ="#009532"
+    leg_left.style.background ="#009532"
+    left_right.style.background ="#009532"
+
+}
+function verdef(){
+    back.style.background = "#007119"
+    shadow_1.style.background = "#007119"
+    shadow.style.background ="#004226"
+    fill.style.background ="#004226"
+    leg_left.style.background ="#004226"
+    left_right.style.background ="#004226"
+
+}function azul(){
+    back.style.background = "#0031e0"
+    shadow_1.style.background = "#0031e0"
+    shadow.style.background ="#001398"
+    fill.style.background ="#001398"
+    leg_left.style.background ="#001398"
+    left_right.style.background ="#001398"
+
+}function verde(){
+    back.style.background = "#00d600"
+    shadow_1.style.background = "#00d600"
+    shadow.style.background ="#009532"
+    fill.style.background ="#009532"
+    leg_left.style.background ="#009532"
+    left_right.style.background ="#009532"
+
+}function amarelo(){
+    back.style.background = "#faec36"
+    shadow_1.style.background = "#faec36"
+    shadow.style.background ="#dd8800"
+    fill.style.background ="#dd8800"
+    leg_left.style.background ="#dd8800"
+    left_right.style.background ="#dd8800"
+
+}function laranja(){
+    back.style.background = "#ff8100"
+    shadow_1.style.background = "#ff8100"
+    shadow.style.background ="#dc4500"
+    fill.style.background ="#dc4500"
+    leg_left.style.background ="#dc4500"
+    left_right.style.background ="#dc4500"
+
+}function preto(){
+    back.style.background = "#34444c"
+    shadow_1.style.background = "#34444c"
+    shadow.style.background ="#171921"
+    fill.style.background ="#171921"
+    leg_left.style.background ="#171921"
+    left_right.style.background ="#171921"
+
+}function branco(){
+    back.style.background = "#d0e1f0"
+    shadow_1.style.background = "#d0e1f0"
+    shadow.style.background ="#7695c2"
+    fill.style.background ="#7695c2"
+    leg_left.style.background ="#7695c2"
+    left_right.style.background ="#7695c2"
+
+}function roxo(){
+    back.style.background = "#7b39c5"
+    shadow_1.style.background = "#7b39c5"
+    shadow.style.background ="#401a82"
+    fill.style.background ="#401a82"
+    leg_left.style.background ="#401a82"
+    left_right.style.background ="#401a82"
+
+}function rosa(){
+    back.style.background = "#ff68bf"
+    shadow_1.style.background = "#ff68bf"
+    shadow.style.background ="#d33cb7"
+    fill.style.background ="#d33cb7"
+    leg_left.style.background ="#d33cb7"
+    left_right.style.background ="#d33cb7"
+
+}function vermelho(){
+    back.style.background = "#f81b02"
+    shadow_1.style.background = "#f81b02"
+    shadow.style.background ="#9a0d35"
+    fill.style.background ="#9a0d35"
+    leg_left.style.background ="#9a0d35"
+    left_right.style.background ="#9a0d35"
+
+}function marrom(){
+    back.style.background = "#83490a"
+    shadow_1.style.background = "#83490a"
+    shadow.style.background ="#722505"
+    fill.style.background ="#722505"
+    leg_left.style.background ="#722505"
+    left_right.style.background ="#722505"
+
+}function ciano(){
+    back.style.background = "#00eadb"
+    shadow_1.style.background = "#00eadb"
+    shadow.style.background ="#009ec4"
+    fill.style.background ="#009ec4"
+    leg_left.style.background ="#009ec4"
+    left_right.style.background ="#009ec4"
+
+}
